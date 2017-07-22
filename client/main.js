0b851ee03874e9d1662e8922bbc7eeb479653794
bonecrushereb <bonecrushereb@gmail.com>
2017-07-22 13:07:21 -0700

    work on rendering names and scores to page

--- client/main.js
+++ client/main.js
@@ -1,5 +1,38 @@
-import someDefault, {greetUser, name} from './../imports/utils';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
 
-console.log('Log from /client/main.js');
-console.log(greetUser());
-console.log(someDefault);
+const players = [
+  {
+    _id: '1',
+    name: 'Ben',
+    score: 60,
+  },
+  {
+    _id: '2',
+    name: 'Pete',
+    score: 10
+  },
+  {
+    _id: '3',
+    name: 'Gail',
+    score: 80
+  }
+];
+
+const renderPlayers = () => {
+  return [<p key="1">1</p>,<p key="2">2</p>,<p key="3">3</p>];
+};
+
+Meteor.startup(() => {
+  let title = 'Score Keep';
+  let name = 'Ben';
+  let jsx = (
+    <div>
+      <h1>{title}</h1>
+      <p>Hello {name}!</p>
+    {renderPlayers()}
+    </div>
+  );
+  ReactDOM.render(jsx, document.getElementById('app'));
+});
