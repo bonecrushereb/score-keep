cbc9d816c7fe8e93121392546a4069ad3d8dff97
bonecrushereb <bonecrushereb@gmail.com>
2017-07-24 10:18:58 -0700

    able to remove player and increment and decrement points

--- server/main.js
+++ server/main.js
@@ -2,9 +2,5 @@ import {Meteor} from 'meteor/meteor';
 import {Players} from './../imports/api/players';
 
 Meteor.startup(() => {
-  // Players.insert({
-  //   name: 'Pete',
-  //   score: 5
-  // });
-  console.log(Players.find().fetch());
+
 });
